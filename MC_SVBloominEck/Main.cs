6776a1b4085343690fd1906e3e61866cf69cd6ef
MPC88 <myemail@gmail.com>
2024-04-13 09:37:08 +0100

    Added range validation/correction.

--- MC_SVBloominEck/Main.cs
+++ MC_SVBloominEck/Main.cs
@@ -18,7 +18,7 @@ namespace MC_SVBloominEck
             cfg_Intensity = Config.Bind<float>("Settings",
                 "Bloom Intensity",
                 0.01f,
-                "");
+                "Range 0.0 to 1.0.");
         }
 
         public void Update()
@@ -26,6 +26,11 @@ namespace MC_SVBloominEck
             if (SetupControl.inst && SetupControl.inst.ppp &&
                 SetupControl.inst.ppp.bloom.settings.bloom.intensity != cfg_Intensity.Value)
             {
+                if (cfg_Intensity.Value < 0)
+                    cfg_Intensity.Value = 0;
+                if (cfg_Intensity.Value > 1.0)
+                    cfg_Intensity.Value = 1.0f;
+
                 BloomModel.Settings bms = SetupControl.inst.ppp.bloom.settings;
                 bms.bloom.intensity = cfg_Intensity.Value;
                 SetupControl.inst.ppp.bloom.settings = bms;
